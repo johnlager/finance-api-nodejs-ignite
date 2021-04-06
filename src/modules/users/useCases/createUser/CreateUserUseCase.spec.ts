e399e20ea915e8ba185eabe95a6112a96cde35b9
Joao Vitor <john.vitor.dss@gmail.com>
2021-04-06 16:19:33 -0300

    test(Unit tests): add CreateUserUseCase unit tests

---
+++ src/modules/users/useCases/createUser/CreateUserUseCase.spec.ts
@@ -0,0 +1,44 @@
+import { InMemoryUsersRepository } from "../../repositories/in-memory/InMemoryUsersRepository";
+import { CreateUserUseCase } from "./CreateUserUseCase";
+import { CreateUserError } from "./CreateUserError";
+
+import { ICreateUserDTO } from "./ICreateUserDTO";
+
+let inMemoryUsersRepository: InMemoryUsersRepository;
+let createUserUseCase: CreateUserUseCase;
+
+describe("Create User", () => {
+  beforeEach(() => {
+    inMemoryUsersRepository = new InMemoryUsersRepository;
+    createUserUseCase = new CreateUserUseCase(inMemoryUsersRepository);
+  });
+
+  it("Should create an user", async () => {
+    const user: ICreateUserDTO = {
+      name: "John",
+      email: "a@b.com",
+      password: "123",
+    };
+
+    const response = await createUserUseCase.execute(user);
+
+    console.log(response)
+
+    expect(response).toHaveProperty("name");
+  });
+
+  it("Should not create an user with an e-mail that is already in use", async () => {
+    
+    expect(async() => {
+      const user: ICreateUserDTO = {
+        name: "John",
+        email: "a@b.com",
+        password: "123",
+      };
+  
+      await createUserUseCase.execute(user);
+      const response = await createUserUseCase.execute(user);  
+    }).rejects.toBeInstanceOf(CreateUserError);
+    
+  })
+})
